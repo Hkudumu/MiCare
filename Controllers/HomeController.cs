c32a48e3885a9d44b5106ffa207111cfbe6d8901
hkudumu94 <hkudumu1994@gmail.com>
2022-07-28 11:41:57 +0530

    DB Changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -47,7 +49,25 @@ namespace MiCare.Controllers
         [HttpPost]
         public IActionResult Home(string fullname,string emailaddress,string username,string phonenumber,string nidnumber, string txtDate)
         {
-            TempData["Msg"] = "Thankyou for Scheduling RTPCR Test";
+            string query = "INSERT INTO dbo.ApprovalFlow (UserId, UserName, UserEmail, UserPhone, UserIdenitity, RegistrationDate) " +
+                   "VALUES (@UserId, @UserName, @UserEmail, @UserPhone, @UserIdenitity, @RegistrationDate) ";
+            string connectionString = "Data Source=.;Initial Catalog=MiCare;User ID =sa; Password =Micron@123";
+            // create connection and command
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                // define parameters and their values
+                cmd.Parameters.Add("@UserName", SqlDbType.VarChar, 50).Value = fullname;
+                cmd.Parameters.Add("@UserEmail", SqlDbType.VarChar, 50).Value = emailaddress;
+                cmd.Parameters.Add("@UserId", SqlDbType.VarChar, 50).Value = username;
+                cmd.Parameters.Add("@UserPhone", SqlDbType.BigInt, 50).Value = Convert.ToInt64(phonenumber);
+                cmd.Parameters.Add("@UserIdenitity", SqlDbType.BigInt).Value = Convert.ToInt64(nidnumber);
+                cmd.Parameters.Add("@RegistrationDate", SqlDbType.Date, 50).Value = txtDate;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                TempData["Msg"] = "Thankyou for Scheduling RTPCR Test";
+            }
             return View();
         }
         public IActionResult Admin()
