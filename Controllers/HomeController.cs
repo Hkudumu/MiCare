d00946f7051654dbf6e7f16c1a2547a668b81e1d
hkudumu94 <hkudumu1994@gmail.com>
2022-08-01 10:44:44 +0530

    Database Code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,27 +19,27 @@ namespace MiCare.Controllers
         {
             _logger = logger;
         }
-       
+
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index(string username,string password)
+        public IActionResult Index(string username, string password)
         {
-                TempData["Name"] = username;
-                if ((username != null && password != null)&&username.ToLower() == "admin" && password.ToLower() == "admin")
-                {
+            TempData["Name"] = username;
+            if ((username != null && password != null) && username.ToLower() == "admin" && password.ToLower() == "admin")
+            {
 
-                    return RedirectToAction("Admin");
+                return RedirectToAction("Admin");
 
-                }
-                else
-                {
-                    return RedirectToAction("Home");
-                }
-            
-           
+            }
+            else
+            {
+                return RedirectToAction("Home");
+            }
+
+
         }
 
         public IActionResult Home()
@@ -49,10 +49,49 @@ namespace MiCare.Controllers
         [HttpPost]
         public IActionResult LoadData()
         {
-            return Json("");
+            var draw = HttpContext.Request.Form["draw"].FirstOrDefault();
+            DataSet ds = new DataSet();
+            List<Employee> list=new List<Employee>();
+            int recordsTotal = 0;
+            string query = "Select * from dbo.ApprovalFlow";
+            string connectionString = "Data Source=.;Initial Catalog=MiCare;User ID =sa; Password =Micron@123";
+            // create connection and command
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                try
+                {
+                    cn.Open();
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    sda.Fill(ds);
+
+                    cn.Close();
+                     list = ds.Tables[0].AsEnumerable()
+                         .Select(dataRow => new Employee
+                         {
+                             UserId = dataRow.Field<string>("UserId"),
+                             UserName = dataRow.Field<string>("UserName"),
+                             UserEmail = dataRow.Field<string>("UserEmail"),
+                             UserPhone = Convert.ToInt64(dataRow.Field<Int64>("UserPhone")),
+                             Aadhar_Number = Convert.ToInt64(dataRow.Field<Int64>("UserIdenitity")),
+                             Registation_Date = Convert.ToDateTime(dataRow.Field<DateTime>("RegistrationDate"))
+                         }).ToList();
+                    recordsTotal = ds.Tables[0].Rows.Count;
+                    TempData["Msg"] = "Thankyou for Scheduling RTPCR Test";
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+              
+                return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = list });
+            }
+           
         }
         [HttpPost]
-        public IActionResult Home(string fullname,string emailaddress,string username,string phonenumber,string nidnumber, string txtDate)
+        public IActionResult Home(string fullname, string emailaddress, string username, string phonenumber, string nidnumber, string txtDate)
         {
             string query = "INSERT INTO dbo.ApprovalFlow (UserId, UserName, UserEmail, UserPhone, UserIdenitity, RegistrationDate) " +
                    "VALUES (@UserId, @UserName, @UserEmail, @UserPhone, @UserIdenitity, @RegistrationDate) ";
---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiCare
+{
+    public class Employee
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public long UserPhone { get; set; }
+
+        public long Aadhar_Number { get; set; }
+
+        public DateTime Registation_Date { get; set; }
+    }
+}
